7602f0926fb89256c0d678365f2e0b5568989664
Pavellow <72201813+Pavellow@users.noreply.github.com>
2023-12-16 21:28:02 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package visitorpackage;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Bienvenue au zoo.");
 		
 		System.out.println("Initialisation d'un soigneur");
